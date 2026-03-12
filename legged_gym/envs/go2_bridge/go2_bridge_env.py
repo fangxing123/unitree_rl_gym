13bac1c3b4f9b450bef905cca82214f61422fa05
fangxing123 <939952794@qq.com>
2026-03-12 21:28:53 +0800

    Fix go2_bridge root state indexing for extra bridge actor

--- legged_gym/envs/go2_bridge/go2_bridge_env.py
+++ legged_gym/envs/go2_bridge/go2_bridge_env.py
@@ -1,15 +1,30 @@
 import os
 
+import numpy as np
 import torch
-from isaacgym import gymapi
+from isaacgym import gymapi, gymtorch
+from isaacgym.torch_utils import get_axis_params, torch_rand_float, quat_rotate_inverse
 from isaacgym.torch_utils import to_torch
 
 from legged_gym import LEGGED_GYM_ROOT_DIR
 from legged_gym.envs.base.legged_robot import LeggedRobot
+from legged_gym.utils.isaacgym_utils import get_euler_xyz as get_euler_xyz_in_tensor
 
 
 class Go2BridgeRobot(LeggedRobot):
     """GO2 bridge task environment with a static narrow bridge actor."""
+
+    def _get_env_origins(self):
+        """Override to avoid torch.meshgrid indexing warning in newer torch."""
+        self.custom_origins = False
+        self.env_origins = torch.zeros(self.num_envs, 3, device=self.device, requires_grad=False)
+        num_cols = np.floor(np.sqrt(self.num_envs))
+        num_rows = np.ceil(self.num_envs / num_cols)
+        xx, yy = torch.meshgrid(torch.arange(num_rows), torch.arange(num_cols), indexing='ij')
+        spacing = self.cfg.env.env_spacing
+        self.env_origins[:, 0] = spacing * xx.flatten()[:self.num_envs]
+        self.env_origins[:, 1] = spacing * yy.flatten()[:self.num_envs]
+        self.env_origins[:, 2] = 0.0
 
     def _create_envs(self):
         """Creates robot envs and an explicit static bridge actor per env."""
@@ -78,6 +93,7 @@ class Go2BridgeRobot(LeggedRobot):
 
         self.bridge_center_y = torch.zeros(self.num_envs, dtype=torch.float, device=self.device, requires_grad=False)
         self.bridge_top_z = torch.zeros(self.num_envs, dtype=torch.float, device=self.device, requires_grad=False)
+        self.robot_actor_indices = torch.zeros(self.num_envs, dtype=torch.long, device=self.device, requires_grad=False)
 
         num_per_row = int(self.num_envs ** 0.5)
         for i in range(self.num_envs):
@@ -109,6 +125,7 @@ class Go2BridgeRobot(LeggedRobot):
 
             self.bridge_center_y[i] = bridge_world[1]
             self.bridge_top_z[i] = bridge_world[2] + bridge_height * 0.5
+            self.robot_actor_indices[i] = self.gym.get_actor_index(env_handle, actor_handle, gymapi.DOMAIN_SIM)
 
             self.envs.append(env_handle)
             self.bridge_handles.append(bridge_handle)
@@ -130,6 +147,99 @@ class Go2BridgeRobot(LeggedRobot):
         self.termination_contact_indices = torch.zeros(len(termination_contact_names), dtype=torch.long, device=self.device, requires_grad=False)
         for i in range(len(termination_contact_names)):
             self.termination_contact_indices[i] = self.gym.find_actor_rigid_body_handle(self.envs[0], self.actor_handles[0], termination_contact_names[i])
+
+    def _init_buffers(self):
+        """Initialize buffers while selecting only robot actors from root states."""
+        actor_root_state = self.gym.acquire_actor_root_state_tensor(self.sim)
+        dof_state_tensor = self.gym.acquire_dof_state_tensor(self.sim)
+        net_contact_forces = self.gym.acquire_net_contact_force_tensor(self.sim)
+        self.gym.refresh_dof_state_tensor(self.sim)
+        self.gym.refresh_actor_root_state_tensor(self.sim)
+        self.gym.refresh_net_contact_force_tensor(self.sim)
+
+        self.all_root_states = gymtorch.wrap_tensor(actor_root_state)
+        self.root_states = self.all_root_states[self.robot_actor_indices]
+        self.dof_state = gymtorch.wrap_tensor(dof_state_tensor)
+        self.dof_pos = self.dof_state.view(self.num_envs, self.num_dof, 2)[..., 0]
+        self.dof_vel = self.dof_state.view(self.num_envs, self.num_dof, 2)[..., 1]
+        self.base_quat = self.root_states[:, 3:7]
+        self.rpy = get_euler_xyz_in_tensor(self.base_quat)
+        self.base_pos = self.root_states[:, 0:3]
+        self.contact_forces = gymtorch.wrap_tensor(net_contact_forces).view(self.num_envs, -1, 3)
+
+        self.common_step_counter = 0
+        self.extras = {}
+        self.noise_scale_vec = self._get_noise_scale_vec(self.cfg)
+        self.gravity_vec = to_torch(get_axis_params(-1.0, self.up_axis_idx), device=self.device).repeat((self.num_envs, 1))
+        self.forward_vec = to_torch([1.0, 0.0, 0.0], device=self.device).repeat((self.num_envs, 1))
+        self.torques = torch.zeros(self.num_envs, self.num_actions, dtype=torch.float, device=self.device, requires_grad=False)
+        self.p_gains = torch.zeros(self.num_actions, dtype=torch.float, device=self.device, requires_grad=False)
+        self.d_gains = torch.zeros(self.num_actions, dtype=torch.float, device=self.device, requires_grad=False)
+        self.actions = torch.zeros(self.num_envs, self.num_actions, dtype=torch.float, device=self.device, requires_grad=False)
+        self.last_actions = torch.zeros(self.num_envs, self.num_actions, dtype=torch.float, device=self.device, requires_grad=False)
+        self.last_dof_vel = torch.zeros_like(self.dof_vel)
+        self.last_root_vel = torch.zeros_like(self.root_states[:, 7:13])
+        self.commands = torch.zeros(self.num_envs, self.cfg.commands.num_commands, dtype=torch.float, device=self.device, requires_grad=False)
+        self.commands_scale = torch.tensor(
+            [self.obs_scales.lin_vel, self.obs_scales.lin_vel, self.obs_scales.ang_vel],
+            device=self.device,
+            requires_grad=False,
+        )
+        self.feet_air_time = torch.zeros(self.num_envs, self.feet_indices.shape[0], dtype=torch.float, device=self.device, requires_grad=False)
+        self.last_contacts = torch.zeros(self.num_envs, len(self.feet_indices), dtype=torch.bool, device=self.device, requires_grad=False)
+        self.base_lin_vel = quat_rotate_inverse(self.base_quat, self.root_states[:, 7:10])
+        self.base_ang_vel = quat_rotate_inverse(self.base_quat, self.root_states[:, 10:13])
+        self.projected_gravity = quat_rotate_inverse(self.base_quat, self.gravity_vec)
+
+        self.default_dof_pos = torch.zeros(self.num_dof, dtype=torch.float, device=self.device, requires_grad=False)
+        for i in range(self.num_dofs):
+            name = self.dof_names[i]
+            angle = self.cfg.init_state.default_joint_angles[name]
+            self.default_dof_pos[i] = angle
+            found = False
+            for dof_name in self.cfg.control.stiffness.keys():
+                if dof_name in name:
+                    self.p_gains[i] = self.cfg.control.stiffness[dof_name]
+                    self.d_gains[i] = self.cfg.control.damping[dof_name]
+                    found = True
+            if not found:
+                self.p_gains[i] = 0.0
+                self.d_gains[i] = 0.0
+                if self.cfg.control.control_type in ['P', 'V']:
+                    print(f'PD gain of joint {name} were not defined, setting them to zero')
+        self.default_dof_pos = self.default_dof_pos.unsqueeze(0)
+
+    def _reset_root_states(self, env_ids):
+        """Reset only robot actor root states (bridge actors are fixed)."""
+        if self.custom_origins:
+            self.root_states[env_ids] = self.base_init_state
+            self.root_states[env_ids, :3] += self.env_origins[env_ids]
+            self.root_states[env_ids, :2] += torch_rand_float(-1.0, 1.0, (len(env_ids), 2), device=self.device)
+        else:
+            self.root_states[env_ids] = self.base_init_state
+            self.root_states[env_ids, :3] += self.env_origins[env_ids]
+
+        self.root_states[env_ids, 7:13] = torch_rand_float(-0.5, 0.5, (len(env_ids), 6), device=self.device)
+
+        actor_ids_int32 = self.robot_actor_indices[env_ids].to(dtype=torch.int32)
+        self.gym.set_actor_root_state_tensor_indexed(
+            self.sim,
+            gymtorch.unwrap_tensor(self.all_root_states),
+            gymtorch.unwrap_tensor(actor_ids_int32),
+            len(actor_ids_int32),
+        )
+
+    def _push_robots(self):
+        """Random pushes only on robot actors."""
+        max_vel = self.cfg.domain_rand.max_push_vel_xy
+        self.root_states[:, 7:9] = torch_rand_float(-max_vel, max_vel, (self.num_envs, 2), device=self.device)
+        actor_ids_int32 = self.robot_actor_indices.to(dtype=torch.int32)
+        self.gym.set_actor_root_state_tensor_indexed(
+            self.sim,
+            gymtorch.unwrap_tensor(self.all_root_states),
+            gymtorch.unwrap_tensor(actor_ids_int32),
+            len(actor_ids_int32),
+        )
 
     def _post_physics_step_callback(self):
         super()._post_physics_step_callback()
